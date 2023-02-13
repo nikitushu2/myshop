4f52972738162d45cfabddac46d7f40baab7e893
Nikita Shub <nikitushu2@gmail.com>
2023-02-13 16:10:48 +0200

    version1

---
+++ orders/views.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render, redirect
+from .models import OrderItem
+from .forms import OrderCreateForm
+from cart.cart import Cart
+from django.urls import reverse
+
+def order_create(request):
+    cart = Cart(request)
+    if request.method == 'POST':
+        form = OrderCreateForm(request.POST)
+        if form.is_valid():
+            order = form.save(commit=False)
+            if cart.coupon:
+                order.coupon = cart.coupon
+                order.discount = cart.coupon.discount
+            order.save()
+            for item in cart:
+                OrderItem.objects.create(order=order,
+                                         product=item['product'],
+                                         price=item['price'],
+                                         quantity=item['quantity'])
+            cart.clear()
+            request.session['order_id'] = order.id
+            return redirect(reverse('payment:process'))
+    else:
+        form = OrderCreateForm()
+    return render(request, 'orders/order/create.html', {'cart': cart, 'form': form})
